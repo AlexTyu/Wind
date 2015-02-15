2e21c67ccbb434edf42de5e77a5355aa73c45abf
AlexTyu <tu@Tus-iMac.local>
2015-02-14 23:21:08 -0800

    Control

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,7 @@ exports = module.exports = function(app) {
 	app.get('/', routes.views.index);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
-	app.get('/gallery', routes.views.gallery);
+	app.get('/faces', routes.views.gallery);
 	app.get('/portfolio', routes.views.portfolio);
 	app.get('/portfolio/:slug', routes.views.portfoliosingle);
 	app.all('/contact', routes.views.contact);
