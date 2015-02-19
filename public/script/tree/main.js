a20e9793008ab0428bd16d1ba4e09561f672ecca
AlexTyu <tu@Tus-iMac.local>
2015-02-18 23:51:08 -0800

    front

--- public/script/tree/main.js
+++ public/script/tree/main.js
@@ -1,7 +1,7 @@
 if (hasWebGLSupportWithExtensions(['OES_texture_float'])) {
     var flow = new Flow(document.getElementById('render'));
 
-    flow.setHue(0.58);
+    flow.setHue(0.54);
     flow.setTimeScale(INITIAL_SPEED);
     flow.setPersistence(INITIAL_TURBULENCE);
 
