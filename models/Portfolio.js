1649c3d0fe1694497afe7566e218622b94a6cdd5
AlexTyu <tu@Tus-iMac.local>
2015-02-23 15:41:43 -0800

    day 8

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -8,7 +8,9 @@ var Portfolio = new keystone.List('portfolio', {
 /* Не трогай сука */
 Portfolio.add({
 	name: { type: String, required: true },
+	description: { type: Types.Html, wysiwyg: true, label: "Short Description", required: true, initial: true },
 	description: { type: Types.Html, wysiwyg: true, label: "Description", required: true, initial: true },
+	description: { type: Types.Html, wysiwyg: true, label: "Description23", required: true, initial: true },
 	image: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 
 		format: function(item, file){
 			return '<img src="/files/'+file.filename+'" style="max-width: 300px">'
