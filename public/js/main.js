2373c50068ddf660113709143a9b7de1bdbb4b84
thwnd <sergey.samus@gmail.com>
2015-02-27 00:25:23 -0800

    ff

--- public/js/main.js
+++ public/js/main.js
@@ -40,5 +40,7 @@ app.controller("portfolioController", function ($scope,$rootScope,$http) {
 $(window).scroll(function(){
 	if ( $( "body" ).hasClass( "portfiliosingle" )  && ($(window).scrollTop() > 500) ){
 		$(".portfiliosingle #header").addClass('bg');
+	} else if ($(window).scrollTop() > 50) {
+		$(".portfiliosingle #header").removeClass('bg');
 	}
 })
