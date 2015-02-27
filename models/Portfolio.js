9f3cbf4c198ea6be4fc75e8bdfcf44db1712554a
thwnd <sergey.samus@gmail.com>
2015-02-26 23:27:56 -0800

    ff

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -11,15 +11,15 @@ Portfolio.add({
 	
 	category: { type: Types.Select, options: 'Advertising, App, Branding, Product Design, Web' },
 	
-	cover: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	cover: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
-	videocover: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 
+	videocover: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 
 	format: function(item, file){return '<source src="/files/'+file.filename+'" type="video/mp4>'}}, 
 	
 	URL: { type: Types.Url, wysiwyg: false, label: "URL", required: false, initial: false},
 	
-	thumbnail: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 
+	thumbnail: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 800px">'}}, 
 	
 	Short: { type: Types.Html, wysiwyg: false, label: "Short Description", required: true, initial: true },
@@ -32,52 +32,52 @@ Portfolio.add({
 	
 	
 	
-	image1: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image1: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image1description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 	
-	image2: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image2: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image2description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 	
-	image3: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image3: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image3description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 	
-	image4: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image4: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image4description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 
-	image5: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image5: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image1description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 	
-	image5: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image5: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image1description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 
-	image6: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image6: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image6description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 	
-	image7: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image7: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image7description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 
-	image8: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image8: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image8description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
 	
-	image9: { type: Types.LocalFile, dest: '../../Projects/Wind/public/files', prefix: '/files/', 	
+	image9: { type: Types.LocalFile, dest: '/home/www/AngarJS/public/files', prefix: '/files/', 	
 	format: function(item, file){return '<img src="/files/'+file.filename+'" style="max-width: 2000px">'}}, 
 	
 	image9description: { type: Types.Html, wysiwyg: false, label: "Image 1 Description", required: false, initial: false },
